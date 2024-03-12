62834c5e39aab1057fa38fe3bb83c0f9163d6db3
aBugInTheSystem <ekom338.com@gmail.com>
2024-03-12 15:10:30 +0300

    Enum for currencies added.

---
+++ LOTRConverter/Currency.swift
@@ -0,0 +1,16 @@
+//
+//  Currency.swift
+//  LOTRConverter
+//
+//  Created by Ekomobong Edeme on 12/03/2024.
+//
+
+
+enum Currency: Double{
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
+
